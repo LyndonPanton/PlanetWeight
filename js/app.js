627c2a0686011efe9a64ab3b64421a3f1f0b9eec
Lyndon Panton <LyndonPanton@gmail.com>
2019-09-04 18:06:19 +0100

    prevented default page refresh on form submission

--- js/app.js
+++ js/app.js
@@ -1,4 +1,9 @@
 window.onload = function(event) {
 	let copyrightYear = document.getElementById("copyright-year");
 	copyrightYear.textContent = (new Date()).getFullYear();
+
+	let form = document.getElementsByTagName("form")[0];
+	form.addEventListener("submit", function(event) {
+		event.preventDefault();
+	});
 };
